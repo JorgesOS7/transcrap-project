bd0da70776588cdd97e9738ad59d28f927fb631f
José Jorge <jjpsilva@viptech.com.br>
2023-08-23 14:09:09 -0300

    ✨ new alert-error component

---
+++ transcrap/src/components/AlertError.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+defineProps<{ message: string }>()
+</script>
+
+<template>
+  <div class="p-2 border border-rose-300 bg-rose-50 rounded">
+    <p class="text-sm font-medium text-rose-500">{{ message }}</p>
+  </div>
+</template>
